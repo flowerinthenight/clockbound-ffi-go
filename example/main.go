4f18fe84e56ce04b0e7a0c23dd0601dbe42987eb
flowerinthenight <root@flowerinthenight.com>
2025-01-28 20:22:51 +0900

    try add include dir in env

---
+++ example/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/flowerinthenight/clockbound-ffi-go"
+)
+
+func main() {
+	cb := clockbound.New()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	done := make(chan error, 1)
+	ticker := time.NewTicker(time.Second * 3)
+	first := make(chan struct{}, 1)
+	first <- struct{}{}
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				done <- nil
+				return
+			case <-first:
+			case <-ticker.C:
+			}
+
+			now, err := cb.Now()
+			if err != nil {
+				log.Println("Now failed:", err)
+				continue
+			}
+
+			log.Printf("earliest: %v\n", now.Earliest.Format(time.RFC3339Nano))
+			log.Printf("latest  : %v\n", now.Latest.Format(time.RFC3339Nano))
+			log.Printf("range: %v\n", now.Latest.Sub(now.Earliest))
+			log.Printf("status: %v\n", now.Status)
+			log.Println("")
+		}
+	}()
+
+	// Interrupt handler.
+	go func() {
+		sigch := make(chan os.Signal, 1)
+		signal.Notify(sigch, syscall.SIGINT, syscall.SIGTERM)
+		log.Println("signal:", <-sigch)
+		cancel()
+	}()
+
+	<-done
+
+	ticker.Stop()
+	cb.Close()
+}
